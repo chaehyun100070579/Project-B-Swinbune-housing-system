601d415db90a42b706a8d61b0d8a5842545c49a0
victorchinyt <4330870@students.swinburne.edu.my>
2018-04-10 23:59:33 +0800

    fixed delete confirm prompt for student

--- hostel/admin/manage-students.php
+++ hostel/admin/manage-students.php
@@ -181,7 +181,7 @@ if(isset($_GET['del']))
 														<a href='edit-student.php?id=$row->id' title='Edit Student'><i class='fa fa-edit'></i>
 														</a>
 														&nbsp;&nbsp;&nbsp;
-														<a href='manage-students.php?del=$row->id' title='Delete Record' onclick='return confirm('Do you want to delete');'>
+														<a href='manage-students.php?del=$row->id' title='Delete Record' onclick='return confirm(\"Do you want to delete\");'>
 															<i class='fa fa-close'></i>
 														</a>
 													  </td>";
