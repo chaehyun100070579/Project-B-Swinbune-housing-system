813575d244fd70c518692f964580d4aae382683f
victorchinyt <4330870@students.swinburne.edu.my>
2018-05-06 23:20:25 +0800

    get_seater.php can show seater again after previous changes
    previous change to sql causes get_seater to not able to get seater value due to dropdown option value changed to room type

--- hostel/get_seater.php
+++ hostel/get_seater.php
@@ -1,18 +1,35 @@
 <?php
 include('includes/pdoconfig.php');
-if(!empty($_POST["roomid"])) 
+if(!empty($_POST["roomid"]))
 {	
 $id=$_POST['roomid'];
-$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+//$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+// SELECT is different because have to put rooms table at the back
+// so that any NULL seater(because no booker) from registration table is not fetched
+// because both tables have 'seater' column and will have 2 after joined
+$query = "SELECT registration.*, rooms.* from rooms
+inner join registration
+on registration.roomno = rooms.room_no
+where rooms.RoomType = :id
+and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+UNION
+select registration.*, rooms.* from rooms
+left outer join registration
+on registration.roomno = rooms.room_no
+where rooms.RoomType = :id
+and registration.roomno IS NULL
+";
+$stmt = $DB_con->prepare($query);
 $stmt->execute(array(':id' => $id));
 ?>
  <?php
- while($row=$stmt->fetch(PDO::FETCH_ASSOC))
- {
+// while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+// {
+ $row=$stmt->fetch(PDO::FETCH_ASSOC);
   ?>
  <?php echo htmlentities($row['seater']); ?>
   <?php
- }
+// }
 }
 
 
@@ -20,16 +37,30 @@ $stmt->execute(array(':id' => $id));
 if(!empty($_POST["rid"])) 
 {	
 $id=$_POST['rid'];
-$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+//$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+$query = "SELECT * from rooms
+inner join registration
+on registration.roomno = rooms.room_no
+where rooms.RoomType = :id
+and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+UNION
+select * from rooms
+left outer join registration
+on registration.roomno = rooms.room_no
+where rooms.RoomType = :id
+and registration.roomno IS NULL
+";
+$stmt = $DB_con->prepare($query);
 $stmt->execute(array(':id' => $id));
 ?>
  <?php
- while($row=$stmt->fetch(PDO::FETCH_ASSOC))
- {
+// while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+// {
+ $row=$stmt->fetch(PDO::FETCH_ASSOC);
   ?>
  <?php echo htmlentities($row['fees']); ?>
   <?php
- }
+// }
 }
 
 
