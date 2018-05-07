ac0f2d253520dd1039cfd407b9f150c9ecf0b4e7
victorchinyt <4330870@students.swinburne.edu.my>
2018-05-08 04:42:48 +0800

    change sql query to include room block (gender) and get seater

--- hostel/get_seater.php
+++ hostel/get_seater.php
@@ -1,27 +1,63 @@
 <?php
 include('includes/pdoconfig.php');
+
+// GET ROOM SEATER AND AUTO FILL IN SEATER
 if(!empty($_POST["roomid"]))
 {
 // roomid is room type
 $id=$_POST['roomid'];
+$gen=$_POST['gender'];
+if($gen == 'female')
+{
+    // if female
+    $gen2 = 'h';
+    $gen3 = '';
+} else {
+    // if male
+    $gen2 = 'hl';
+    $gen3 = 'hm';
+}
 //$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
 // SELECT is different because have to put rooms table at the back
 // so that any NULL seater(because no booker) from registration table is not fetched
 // because both tables have 'seater' column and will have 2 after joined
+
+/*
+// !!! OLD QUERY WITHOUT ROOM BLOCK H, HL, HM !!!
+// $query = "SELECT registration.*, rooms.* from rooms
+// inner join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+// UNION
+// select registration.*, rooms.* from rooms
+// left outer join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and registration.roomno IS NULL
+// ";
+*/
 $query = "SELECT registration.*, rooms.* from rooms
 inner join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
-and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+and (rooms.block = :gender2 or rooms.block = :gender3)
+and (select count(*) from rooms inner join registration on registration.roomno = rooms.room_no 
+where rooms.RoomType = :id and (rooms.block = :gender2 or rooms.block = :gender3)) < rooms.seater
 UNION
 select registration.*, rooms.* from rooms
 left outer join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
+and (rooms.block = :gender2 or rooms.block = :gender3)
 and registration.roomno IS NULL
 ";
 $stmt = $DB_con->prepare($query);
-$stmt->execute(array(':id' => $id));
+$stmt->execute(array(
+    ':id' => $id,
+    ':gender2' => $gen2,
+    ':gender3' => $gen3
+    ));
 ?>
  <?php
 // while($row=$stmt->fetch(PDO::FETCH_ASSOC))
@@ -35,25 +71,60 @@ $stmt->execute(array(':id' => $id));
 
 
 
+
+// GET ROOM FEES AND AUTO FIlL IN WEEKLY FEES
 if(!empty($_POST["rid"])) 
 {
 //rid is room type
 $id=$_POST['rid'];
+$gen=$_POST['gender'];
+if($gen == 'female')
+{
+    // if female
+    $gen2 = 'h';
+    $gen3 = '';
+} else {
+    // if male
+    $gen2 = 'hl';
+    $gen3 = 'hm';
+}
 //$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+/*
+// !!! OLD QUERY WITHOUT ROOM BLOCK H, HL, HM !!!
+// $query = "SELECT * from rooms
+// inner join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+// UNION
+// select * from rooms
+// left outer join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and registration.roomno IS NULL
+// ";
+*/
 $query = "SELECT * from rooms
 inner join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
-and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+and (rooms.block = :gender2 or rooms.block = :gender3)
+and (select count(*) from rooms inner join registration on registration.roomno = rooms.room_no 
+where rooms.RoomType = :id and (rooms.block = :gender2 or rooms.block = :gender3)) < rooms.seater
 UNION
 select * from rooms
 left outer join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
+and (rooms.block = :gender2 or rooms.block = :gender3)
 and registration.roomno IS NULL
 ";
 $stmt = $DB_con->prepare($query);
-$stmt->execute(array(':id' => $id));
+$stmt->execute(array(
+    ':id' => $id,
+    ':gender2' => $gen2,
+    ':gender3' => $gen3
+    ));
 ?>
  <?php
 // while($row=$stmt->fetch(PDO::FETCH_ASSOC))
@@ -65,25 +136,62 @@ $stmt->execute(array(':id' => $id));
 // }
 }
 
+
+
+
+// GET ROOM NUMBER AND REPLACE VALUE OF DROP DOWN OPTION
 if(!empty($_POST["getroomnum"])) 
 {
 //getroomnum is room type
 $id=$_POST['getroomnum'];
+$gen=$_POST['gender'];
+if($gen == 'female')
+{
+    // if female
+    $gen2 = 'h';
+    $gen3 = '';
+} else {
+    // if male
+    $gen2 = 'hl';
+    $gen3 = 'hm';
+}
 //$stmt = $DB_con->prepare("SELECT * FROM rooms WHERE room_no = :id");
+/*
+// !!! OLD QUERY WITHOUT ROOM BLOCK H, HL, HM !!!
+// $query = "SELECT registration.*, rooms.* from rooms
+// inner join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+// UNION
+// select registration.*, rooms.* from rooms
+// left outer join registration
+// on registration.roomno = rooms.room_no
+// where rooms.RoomType = :id
+// and registration.roomno IS NULL
+// ";
+*/
 $query = "SELECT registration.*, rooms.* from rooms
 inner join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
-and (select count(*) from rooms inner join registration where rooms.RoomType = :id and registration.roomno = rooms.room_no) < rooms.seater
+and (rooms.block = :gender2 or rooms.block = :gender3)
+and (select count(*) from rooms inner join registration on registration.roomno = rooms.room_no 
+where rooms.RoomType = :id and (rooms.block = :gender2 or rooms.block = :gender3)) < rooms.seater
 UNION
 select registration.*, rooms.* from rooms
 left outer join registration
 on registration.roomno = rooms.room_no
 where rooms.RoomType = :id
+and (rooms.block = :gender2 or rooms.block = :gender3)
 and registration.roomno IS NULL
 ";
 $stmt = $DB_con->prepare($query);
-$stmt->execute(array(':id' => $id));
+$stmt->execute(array(
+    ':id' => $id,
+    ':gender2' => $gen2,
+    ':gender3' => $gen3
+    ));
 ?>
  <?php
 // while($row=$stmt->fetch(PDO::FETCH_ASSOC))
@@ -95,6 +203,10 @@ $stmt->execute(array(':id' => $id));
 // }
 }
 
+
+
+
+// GET DURATION OF COURSE CODE AND AUTO FILL IN DURATION (WEEKS)
 if(!empty($_POST["course_code"])) 
 {	
 $id=$_POST['course_code'];
