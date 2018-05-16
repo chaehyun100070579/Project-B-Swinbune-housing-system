427c010290314ed65b8185a315355d32e8a77153
chaehyun100070579 <32354521+chaehyun100070579@users.noreply.github.com>
2018-05-16 16:01:14 +0800

    delete "others" option list for gender selection

--- hostel/registration.php
+++ hostel/registration.php
@@ -124,7 +124,6 @@ if(isset($_POST['submit']))
                                                             <option value="">Select Gender</option>
                                                             <option value="male">Male</option>
                                                             <option value="female">Female</option>
-                                                            <option value="others">Others</option>
                                                         </select>
                                                     </div>
                                                 </div>
